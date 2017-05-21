069c8607660fadcbed0fa29c051c92869e321abc
Zic <doquangdat_t59@hus.edu.vn>
2017-05-21 16:28:41 +0700

    create enviroment file and connect to db

--- main.cpp
+++ main.cpp
@@ -4,29 +4,52 @@
 #include <QDebug>
 #include <QSqlError>
 #include <QSqlQuery>
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setPort(3305);
-    db.setHostName("127.0.0.1");
-    db.setDatabaseName("Bot");
-    db.setUserName("chatbot");
-    db.setPassword("123456");
+#include <QFile>
+#include <QTextStream>
 
-    if(db.open()){
-        QSqlQuery q(db);
-        if(q.exec("select * from LichHoc where 1")){
-            while(q.next()){
-                qDebug() << q.value(2).toString();
-            }
+QSqlDatabase db;
+void create_connect(){
+    QFile file("/home/zic/Documents/C++Code/ChatBot/.env");
+    if(!file.open(QIODevice::ReadOnly)){
+        qDebug() << file.errorString();
+        file.close();
+    } else {
+        QTextStream read(&file);
+        QString port = read.readLine().split("=").at(1);
+        QString url = read.readLine().split("=").at(1);
+        QString db_name = read.readLine().split("=").at(1);
+        QString db_user = read.readLine().split("=").at(1);
+        QString db_pass = read.readLine().split("=").at(1);
+        db = QSqlDatabase::addDatabase("QMYSQL");
+        db.setPort(port.toInt());
+        db.setHostName(url);
+        db.setDatabaseName(db_name);
+        db.setUserName(db_user);
+        db.setPassword(db_pass);
+
+        if(db.open()){
+//            QSqlQuery q(db);
+//            if(q.exec("select * from LichHoc where 1")){
+//                while(q.next()){
+//                    qDebug() << q.value(2).toString();
+//                }
+//               } else {
+//                qDebug() << db.lastError().text();
+//            }
+            qDebug() << "Connect success!!";
         } else {
             qDebug() << db.lastError().text();
         }
-    } else {
-        qDebug() << db.lastError().text();
     }
+
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    create_connect();
+    MainWindow w;
+    w.show();
+
     return a.exec();
 }
